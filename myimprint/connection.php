f15122c4792a33b200ad43c5723bdf9d27943a7f
Harshit Mishra <harshitmishra1331@gmail.com>
2017-06-06 18:40:17 +0530

    Add files via upload

---
+++ myimprint/connection.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_NAME','myimprint');
+define('DB_USER','root');
+define('DB_PASSWORD',''); 
+define('DB_HOST','localhost');
+
+$link=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
+if(!$link)
+{
+	die('could not connect:' .mysql_error());
+}
+$db_selected=mysql_select_db(DB_NAME,$link);
+
+?>
